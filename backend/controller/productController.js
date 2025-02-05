17b6a13067d3bd1743da8bfd4532c4aa70e0334c
yuke001 <yukenthirans2018@gmail.com>
2025-02-05 15:48:36 +0530

    create api's for get, patch,delete

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,5 +1,7 @@
+import mongoose from "mongoose";
 import Product from "../models/product.model.js";
 
+// Create a new product
 export const postProduct = async (req, res) => {
   const product = req.body;
 
@@ -17,3 +19,63 @@ export const postProduct = async (req, res) => {
     res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
+
+
+// Get all products
+export const getProducts = async (req , res)=>{
+    try {
+        const products = await Product.find({});
+        res.status(200).json({success: true, products})
+    } catch (error) {
+        console.error("Error in getProducts :", error.message);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+}
+
+// Get a single product
+export const getProduct = async(req,res)=>{
+    let {id} = req.params
+    try {
+        const product = await Product.findById(id)
+        res.status(200).json({success: true, product})
+    } catch (error) {
+        console.error("Error in getProduct :", error.message);
+        res.status(500).json({ success: false, message: "Product not found" });
+    }
+}
+
+// Update a product
+
+export const updateProduct = async(req,res)=>{
+    let {id} = req.params
+    const product = req.body
+    
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({success: false, message: "Invalid product ID"})
+    }
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true})
+        res.status(200).json({success: true, updatedProduct})
+    }
+    catch (error) {
+        console.error("Error in updateProduct :", error.message);
+        res.status(500).json({ success: false, message: "Product not found" });
+    }
+}
+
+
+
+// Delete a product
+export const deleteProducts = async(req,res)=>{
+    let {id} = req.params
+
+    try {
+        await Product.findByIdAndDelete(id)
+        res.status(200).json({success: true, message: "Product deleted successfully"})
+    } catch (error) {
+        console.error("Error in deleteProducts :", error.message);
+        res.status(500).json({ success: false, message: "Product not found" });
+        
+    }
+
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { postProduct } from "../controller/productController.js";
+import { deleteProducts, getProduct, getProducts, postProduct, updateProduct } from "../controller/productController.js";
 
 let router = Router();
 
 router.post("/products", postProduct);
+router.get("/products", getProducts);
+
+router.get("/products/:id", getProduct);
+
+router.patch("/products/:id", updateProduct);
+
+router.delete("/products/:id", deleteProducts);
 
 export default router;
