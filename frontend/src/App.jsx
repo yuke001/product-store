ff9f4a28d7989255b7d8bce7472bd05b779b12ac
yuke001 <yukenthirans2018@gmail.com>
2025-02-06 15:45:17 +0530

    frontend - add create page with routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,20 @@
 import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./component/HomePage";
-import CreatePage from "./component/CreatePage";
-import Navbar from "./component/Navbar";
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
 
-const App = () => {
-  return (
-    <>
-      <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
-        <Navbar></Navbar>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/create" element={<CreatePage />}></Route>
-        </Routes>
-      </Box>
-    </>
-  )
+function App() {
+	return (
+		<Box minH={"100vh"} bg={useColorModeValue("gray.50", "gray.900")}>
+			<Navbar></Navbar>
+			<Routes>
+				<Route path='/' element={<HomePage></HomePage>} />
+				<Route path='/create' element={<CreatePage />} />
+			</Routes>
+		</Box>
+	);
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,15 @@
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import * as ReactDOM from 'react-dom/client'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
-const rootElement = document.getElementById('root')
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
         <App />
       </ChakraProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
